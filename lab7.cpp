52a276feab8ad22cc884c848c1c399731aa5b5aa
joelrnorris <joel.r.norris@gmail.com>
2017-04-19 21:00:29 -0500

    finished ola7. works.

--- lab7.cpp
+++ lab7.cpp
@@ -1,5 +1,12 @@
-// lab7.cpp : Defines the entry point for the console application.
-//
+/* Name: Norris, Joel R.
+ * Class: CSCI_3080
+ * Instructor: Dr. Pettey
+ * Title: Lab 7
+ * Description:
+ * read in the number of nodes and a binary relation representing a graph.  
+ * The program will create an adjacency matrix from the binary relation.  
+ * The program will then print the adjacency matrix and whether or not an Euler path exists. 
+ */
 
 #include <iostream>
 
@@ -17,21 +24,27 @@ void printAdjacencyMatrix(int A[][COLS], int numNodes);
 
 int main()
 {
+	// get user data, number of nodes and binary relation
 	int numNodes = getNumNodes();
 	getBinaryRelation(A);
 	cout << endl;
+
+	// output the adjacency matrix
 	printAdjacencyMatrix(A, numNodes);
 
+	// determine is a euler path exists and output result
 	if (numOddNodes == 0 || numOddNodes == 2) {
 		cout << "An Euler path does exist in the graph" << endl;
 	}
 	else {
-		cout << "An Euler path does not exist in the graph" << endl;
+		cout << "An Euler path does NOT exist in the graph" << endl;
 	}
 
     return 0;
 }
 
+
+// get user data for number of nodes in the graph
 int getNumNodes() {
 	int numNodes;
 	cout << "How many nodes are in the graph? ";
@@ -39,6 +52,8 @@ int getNumNodes() {
 	return numNodes;
 }
 
+
+// get user data in the form of a binary relation
 void getBinaryRelation(int A[][COLS]) {
 	
 	char character;
@@ -49,6 +64,9 @@ void getBinaryRelation(int A[][COLS]) {
 	cout << "Please input the binary relation for the graph:" << endl;
 	cin >> character;	// ditch the '{'
 
+	// loop through the rest of the input and build out the adjacency matrix on the fly
+	// discard the characters { , ( )
+	// just grab the numeric values and use those to populate the rows / columns
 	while (character != '}') {
 
 		cin >> character;
@@ -57,32 +75,35 @@ void getBinaryRelation(int A[][COLS]) {
 		cin >> int(col);
 		cin >> character >> character;
 
-		A[row - 1][col - 1] = 1 ;
+		A[row - 1][col - 1] = 1 ;		// offset for array index count
 
 	}
 
 
 }
 
+
+// output the adjacency matrix and check the number of odd nodes at the same time
 void printAdjacencyMatrix(int A[][COLS], int numNodes) {
 
-	cout << "The Adjacency Matrix is: " << endl;
+	int nodeParity = 0;		// track node parity for each row / node
 
-	int nodeParity = 0;
+	for (int i = 0; i < numNodes; ++i) {		// rows
 
-	for (int i = 0; i < numNodes; ++i) {
+		nodeParity = 0;
 
-		for (int j = 0; j < numNodes; ++j) {
+		for (int j = 0; j < numNodes; ++j) {	// cols
 			
-			if (A[i][i] == 1) nodeParity += 2;			// check parity on the diagonal
-			else if (A[i][j] == 1) nodeParity += 1;		// 
+			if (A[i][i] == 1) nodeParity += 2;			// check parity on the diagonal, a loop adds 2 to the parity count
+			else if (A[i][j] == 1) nodeParity += 1;		// otherwise it's just a single connection, 
 
 			cout << A[i][j] << " ";
 
 		}
 
-		if (nodeParity % 2 == 1) ++numOddNodes;
+		if ((nodeParity % 2) == 1) ++numOddNodes;			// if the node parity is odd update the count of odd nodes
 		cout << endl;
+
 	}
 	cout << endl;
 }
